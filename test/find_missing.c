7fb55d8cb5b1ac113e6add65d04853482417a06b
supratik <supratik.dnit@gmail.com>
2016-07-11 20:16:26 -0700

    find missing

--- test/find_missing.c
+++ test/find_missing.c
@@ -3,26 +3,39 @@
 #include <unistd.h>
 
 
-int find(char * arr, int len) {
+int find(int * arr, int len) {
 
-    if (arr[len - 1] == arr[0] + len - 1)
+    int low = 0;
+    int high = len - 1;
+    int mid = 0;
+
+    if (arr[low] + (high - low) == arr[high])
 	return 0;
 
-    while (1) {
-	if (arr[0] == arr[(len/2) - 1])
+    while (low + 1 < high) {
+
+	mid = (low + high) / 2;
+
+	if (arr[low] + (mid - low) == arr[mid]) {
+	    // continue in 2nd half
+	    low = mid;
+	} else {
+	    // continue in 1st half
+	    high = mid;
+	}
+
     }
 
-    return 0;
+    return arr[low] + 1;
 }
 
 
 int main () {
-    int arr[][] = {{4,5,6,7,8},   // no missing
-	           {4,6,7,8,9},   //  5 missing
-	           {4,5,7,8,9},   //  6 missing
-	           {4,5,6,8,9},   //  7 missing
-	           {4,5,6,7,9},   //  8 missing
-                             };
+    int arr[5][5] = {{4,5,6,7,8},   // no missing
+	             {4,6,7,8,9},   //  5 missing
+	             {4,5,7,8,9},   //  6 missing
+	             {4,5,6,8,9},   //  7 missing
+	             {4,5,6,7,9}};  //  8 missing
 
     int m = 99999;
 
