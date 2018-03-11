12ece0aec1d5675fffe68f07a117788c3f87a9df
supratik singha <supratik.dnit@gmail.com>
2018-03-11 16:44:59 -0700

    .

---
+++ scripts/Restart WIFI.bat
@@ -0,0 +1,5 @@
+echo Disable Wireless Network Connection
+netsh interface set interface "Wireless Network Connection" Disable
+echo Enable Wireless Network Connection
+netsh interface set interface "Wireless Network Connection" Enable
+echo Done
