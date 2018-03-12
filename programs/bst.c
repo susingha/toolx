147dc3ceea890aeaa9b719badfd8ddc302d1c947
supratik singha <supsingh@cisco.com>
2018-03-12 14:20:14 -0700

    .

--- programs/bst.c
+++ programs/bst.c
@@ -7,6 +7,9 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+#define TRUE 1
+#define FALSE 0
+
 
 struct node {
     int num;
@@ -15,7 +18,9 @@ struct node {
 };
 
 
-struct node * insert(struct node * nodeHead, int num)
+// Traverses a binary search tree and inserts at a leaf position
+struct node *
+insertiter(struct node * nodeHead, int num)
 {
     struct node * nodeNew = (struct node *)malloc(sizeof(struct node));
     nodeNew->num = num;
@@ -26,30 +31,29 @@ struct node * insert(struct node * nodeHead, int num)
         // this is the first node
         return nodeNew;
     } else {
-        // first node exists. go to the required place and fix it there
-        do {
-            if(num < nodeThis->num) {
-                // go left and check for NULL
-                if(nodeThis->lptr) {
+        while (nodeThis) {
+            if (num < nodeThis->num) {
+                if (nodeThis->lptr) {
                     nodeThis = nodeThis->lptr;
                 } else {
                     nodeThis->lptr = nodeNew;
                     break;
                 }
-            } else if(num >= nodeThis->num) {
-                // go right and check for NULL
-                if(nodeThis->rptr) {
+            } else {
+                if (nodeThis->rptr) {
                     nodeThis = nodeThis->rptr;
                 } else {
                     nodeThis->rptr = nodeNew;
                     break;
                 }
             }
-        } while (1);
+        }
     }
     return nodeHead;
 }
 
+// Recursive traversal and insertion. More homogeneus code: Torvalds Style
+// Passes address of head pointer during recursive call.
 void
 insertrec1(struct node ** nodeHead_, int num)
 {
@@ -71,13 +75,19 @@ insertrec1(struct node ** nodeHead_, int num)
     }
 }
 
+// Recursive traversal and insertion. Messeir code then insertrec1
+// Passes the head pointer during recursive call.
 struct node *
 insertrec2(struct node * nodeHead, int num)
 {
+    struct node * nodeNew = NULL;
+
     if(!nodeHead) {
-        struct node * nodeNew = (struct node *)malloc(sizeof(struct node));
+#ifndef ALLOC
+        nodeNew = (struct node *)malloc(sizeof(struct node));
         nodeNew->num = num;
         nodeNew->lptr = nodeNew->rptr = NULL;
+#endif
         return nodeNew;
     } else {
         struct node * nodeThis = nodeHead;
@@ -85,18 +95,22 @@ insertrec2(struct node * nodeHead, int num)
             if(nodeThis->lptr) {
                 insertrec2(nodeThis->lptr, num);
             } else {
-                struct node * nodeNew = (struct node *)malloc(sizeof(struct node));
+#ifndef ALLOC
+                nodeNew = (struct node *)malloc(sizeof(struct node));
                 nodeNew->num = num;
                 nodeNew->lptr = nodeNew->rptr = NULL;
+#endif
                 nodeThis->lptr = nodeNew;
             }
         } else if(num >= nodeThis->num) {
             if(nodeThis->rptr) {
                 insertrec2(nodeThis->rptr, num);
             } else {
-                struct node * nodeNew = (struct node *)malloc(sizeof(struct node));
+#ifndef ALLOC
+                nodeNew = (struct node *)malloc(sizeof(struct node));
                 nodeNew->num = num;
                 nodeNew->lptr = nodeNew->rptr = NULL;
+#endif
                 nodeThis->rptr = nodeNew;
             }
         }
@@ -104,27 +118,31 @@ insertrec2(struct node * nodeHead, int num)
 }
 
 
+// Inorder Traversal and display
 // this function could be change to return the number of nodes displayed.
 // good excercise in a recursive scenario
-void displayrec(struct node * nodeHead)
+void displayrec(struct node * nodeHead, int outer)
 {
-    // this is in inorder
-    if(!nodeHead) {
+    struct node *nodeThis;
+
+    if(!nodeHead)
         return;
-    }
 
-    struct node * nodeThis = nodeHead;
+    if(outer)
+        printf("--- Display ---\n");
 
-    displayrec(nodeThis->lptr);
+    nodeThis = nodeHead;
+    displayrec(nodeThis->lptr, FALSE);
     printf("%d, ", nodeThis->num);
-
-    displayrec(nodeThis->rptr);
+    displayrec(nodeThis->rptr, FALSE);
 }
 
 
-void delete(struct node * head, int find) {
+// Needs work. Unused
+#ifdef UNUSED
+void deleteiter(struct node * head, int find) {
     int prev;
-    struct node * delnode = NULL, pred = NULL;
+    struct node *delnode = NULL, *pred = NULL;
     prev = displayiter(head, 63, delnode, pred);
 
     // we are trying to delete 63.
@@ -134,8 +152,12 @@ void delete(struct node * head, int find) {
 	// we need the parent pointer too and whether this is a left or right child
     }
 }
+#endif
 
 
+// Core Idea: Use a stack push and go left, pop and go right
+// Needs work. Unused
+#ifdef UNUSED
 int displayiter(struct node * nodeHead, int find, struct node ** delnode, struct node ** pred)
 {
     struct node * this = NULL;
@@ -147,6 +169,14 @@ int displayiter(struct node * nodeHead, int find, struct node ** delnode, struct
 
     // this is in inorder
     this = nodeHead;
+
+    while (this) {
+        if (this->lptr) {
+            push(this);
+        }
+
+    }
+
     while(this) {
 	if(this->lptr && new) {
 	    arr[++top] = this;
@@ -184,28 +214,27 @@ int displayiter(struct node * nodeHead, int find, struct node ** delnode, struct
     }
     return prev;
 }
+#endif
 
 #define rnd() (rand()%100)
 int main()
 {
     struct node * head = NULL;
-    displayrec(head);
-    printf("\n\n");
+    displayrec(head, TRUE);
 #if 0
-    head = insert(head, 5);
-    insert(head, 1);
-    insert(head, 8);
+    head = insertiter(head, 5);
+    insertiter(head, 1);
+    insertiter(head, 8);
 #endif
-    head = insertrec2(head, rnd());
-    insert(head, rnd());
-    insertrec2(head, rnd());
-    insert(head, rnd());
-    insert(head, rnd());
     insertrec1(&head, rnd());
-    insert(head, rnd());
-    insert(head, rnd());
-    insert(head, rnd());
-    insert(head, rnd());
+    insertiter(head, rnd());
+    insertrec2(head, rnd());
+    insertiter(head, rnd());
+    insertiter(head, rnd());
+    insertiter(head, rnd());
+    insertiter(head, rnd());
+    insertiter(head, rnd());
+    insertiter(head, rnd());
     insertrec2(head, 7);
     insertrec1(&head, 5);
     insertrec2(head, 6);
@@ -228,12 +257,13 @@ int main()
     insertrec2(head, rnd());
     insertrec2(head, rnd());
     insertrec2(head, rnd());
-    printf("\n--- display --- \n");
-    displayrec(head);
-    printf("\n--- display --- \n");
-    displayiter(head, 5);
-    printf("\n");
 
+    displayrec(head, TRUE);
+#if 0
+    displayiter(head, 5);
+#endif
+
+    printf("\n");
     return 0;
 }
 
