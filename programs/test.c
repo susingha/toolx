e3e4eac90a12bee6ba74b1c846149f06d23007bf
supratik <supratik.dnit@gmail.com>
2018-05-31 17:28:34 -0700

    replicating C progs to Py

--- programs/test.c
+++ programs/test.c
@@ -1,54 +1,86 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct mov {
-    int a;
-    char p1[0];   // doesnt count (no padding)
-    int b;
-    char p2[3];   // counts as 4 bytes (1 byte padding)
-    int c;
-    short int sd; // counts as 4 bytes (2 byte padding)
-    int d;
-};
-
-union uu {
-    int a;
-    char p1;
-};
-
-int main() {
-    struct mov m;
-    m.a = 56;
-    int arr[] = {1,2,3,4,5};
-    int i = 4, j = 3;
-
-    char * str1 = "ABC";
-    char str2[] = "DEF";
-
-    struct mov * mp = NULL;
-    struct mov * mq = mp + 1;
-    printf("%u\n", mp);
-    printf("%u\n", mq);
-    printf("size = %ld\n", ((unsigned long int)mq - (unsigned long int)mp));
-
-    printf("unsigned long = %d, struct = %d, union = %d, array = %d, long long int = %d\n", (int) sizeof(unsigned long),
-	                                                              (int) sizeof(m),
-							              (int) sizeof(union uu),
-							              (int) sizeof(arr),
-								      (int) sizeof(long long int));
-
-    printf("%s \n%s \n", str1, str2);
-    *str1 = 'X';
-    *str2 = 'Y';
-    printf("%s \n%s \n", str1, str2);
-
-
-    printf("%f", (float)i/(float)j);
-
-
-    printf("\n\n");
-    return 0;
+void display(int arr[], int size) {
+    int i = 0;
+    for (i = 0; i < size; ++i) {
+	printf("%2d, ", arr[i]);
+    }
+    printf("\n");
 }
+
+
+void merge(int arr1[], int len1,
+           int arr2[], int len2,
+           int arr3[]) {
+
+    int i = 0, j = 0, k = 0;
+
+    while (i < len1 && j < len2) {
+
+	if (arr1[i] < arr2[j] || arr1[i] == arr2[j]) {
+	    arr3[k] = arr1[i];
+	    i++;
+	    k++;
+	} else {
+	    arr3[k] = arr2[j];
+	    j++;
+	    k++;
+	}
+    }
+
+    while (i < len1) {
+	arr3[k] = arr1[i];
+	i++;
+	k++;
+    }
+
+    while (j < len2) {
+	arr3[k] = arr2[j];
+	j++;
+	k++;
+    }
+}
+
+void msort(int arr1[], int b, int e, int arr2[]) {
+    int m;
+
+    if (b >= e) {
+	return;
+    }
+
+    m = (b + e)/2;
+    msort(arr2, b,   m, arr1);
+    msort(arr2, m+1, e, arr1);
+
+    merge(&arr1[b],   m-b+1,
+	  &arr1[m+1], e-(m+1)+1,
+	  &arr2[b]);
+}
+
+#define MAX 20
+int array1[MAX];
+int array2[MAX];
+
+int main () {
+    int i = 0;
+    int num = 0;
+    srand(22);
+    for (i = 0; i < MAX; ++i) {
+	num = rand()%100;
+	array1[i] = num;
+	array2[i] = num;
+    }
+
+    display(array1, MAX);
+    display(array2, MAX);
+
+    msort(array1, 0, MAX-1, array2);
+//  display(array1, MAX);
+    display(array2, MAX);
+
+}
+
 
 
 
