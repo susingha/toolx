0335d39cbfced54a39680238bd7e3ad831f3c3f1
supratik <supratik.dnit@gmail.com>
2018-10-02 21:33:04 -0700

    adding cooler methods

--- programs/matrixRotate.c
+++ programs/matrixRotate.c
@@ -34,54 +34,33 @@ void printMatrix() {
 
 
 /*
- * Method 1. Sweep each element along the circumference
+ * Method 1. Sweep each quardrant along the circumference
  *           Like a clock arm. More efficient than method 2
  */
 
-void rotate90 (int x, int y, int *i, int *j) {
-    int oi = x;
-    int oj = y;
-    int ni = ZERO + oj;
-    int nj = LIMT - oi;
-    *i = ni;
-    *j = nj;
-}
+void sweepcircle(int row, int col) {
+    int t1, t2, t3, t4;
 
+    t1 = arr[row][col];
+    t2 = arr[col][ARRMAX-1-row];
+    t3 = arr[ARRMAX-1-row][ARRMAX-1-col];
+    t4 = arr[ARRMAX-1-col][row];
 
-void get_final(int rotations, int row1, int col1, int *row2p, int *col2p) {
-    for (int i = 0; i < rotations; ++i) {
-	rotate90 (row1, col1, row2p, col2p);
-	row1 = *row2p;
-	col1 = *col2p;
-    }
+    arr[row][col] = t4;
+    arr[col][ARRMAX-1-row] = t1;
+    arr[ARRMAX-1-row][ARRMAX-1-col] = t2;
+    arr[ARRMAX-1-col][row] = t3;
 }
 
 void rotateclockarm() {
+    int i, j;
 
-    int save = 0, move = 0;
-    int i = 0, j = 0, k = 0;
-    int begin = 0, end = 0;
-    int row1 = 0, col1 = 0, row2 = 0, col2 = 0;
+    /* only pick elements from the quardrant, right of a half diagonal */
 
-    for (i = 0; i < (ARRMAX)/2; ++i) {
-	begin = ZERO + i;
-	end   = LIMT - i;
-	if(debug) printf("servicing: ");
-	for (j = begin ; j < end; ++j) {
-	    row1 = begin;
-	    col1 = j;
-	    if(debug) printf("%d:[%d][%d], ", arr[row1][col1], row1, col1);
-	    save = arr[row1][col1];
-	    for (k = 0; k < FOUR; ++k) {
-		move = save;
-		get_final(ONE, row1, col1, &row2, &col2);
-		save = arr[row2][col2];
-		arr[row2][col2] = move;
-		row1 = row2;
-		col1 = col2;
-	    }
+    for (i = 0; i < ARRMAX-1; ++i) {
+	for (j = i; j < ARRMAX-1-i; ++j) {
+	    sweepcircle(i, j);
 	}
-	if(debug) printf("\n");
     }
 }
 
@@ -136,11 +115,11 @@ int main() {
     initMatrix();
     printMatrix();
 
-    printf("Using Method 1\n");
+    printf("Using Method 1:\n");
     rotateclockarm();
     printMatrix();
 
-    printf("Using Method 2\n");
+    printf("Using Method 2:\n");
     rotatespinaxis();
     printMatrix();
 
