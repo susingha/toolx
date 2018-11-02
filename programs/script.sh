f442d014d26a811edc06abb2c7af362f1e2275f2
supratik <supratik.dnit@gmail.com>
2018-11-02 00:09:49 -0700

    adding compute program

---
+++ programs/script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# start..end..increment
+for i in {1..50}; do
+    ./a.out $i
+done
+
--- programs/test.c
+++ programs/test.c
@@ -3,7 +3,6 @@
 #include <unistd.h>
 
 #if 0
-#include <time.h>
 #include <string.h>
 #include <stdint.h>
 #include <sys/types.h>
@@ -13,6 +12,31 @@
 #define FALSE 0
 #define ENDS  ('\0')
 
+#define MEASURE_RUNTIME
+#ifdef MEASURE_RUNTIME
+#include <time.h>
+clock_t ts;
+#endif
+
+#define RANDOM
+#ifdef RANDOM
+#define rnd() (rand()%100)
+#else
+#define rnd() (dataset[dataindex++])
+#endif
+
+int * getarr(int n) {
+    int * arr = calloc(n, sizeof(int));
+    srand(time(NULL));
+    while (n--) {
+	arr[n] = rnd();
+	printf("%d, ", arr[n]);
+    }
+    printf("\n");
+    return arr;
+}
+
+
 int main (int argc, char *argv[])
 {
     int c;
@@ -21,6 +45,18 @@ int main (int argc, char *argv[])
     }
     printf("(%d)\n", argc);
 
+#ifdef MEASURE_RUNTIME
+    ts = clock();
+    unsigned int i = 0, j;
+    j = --i;
+    while(--i);
+    ts = clock() - ts;
+    printf("Counted %u to %u in %f seconds\n", j, i, ((double)ts) / CLOCKS_PER_SEC); 
+#endif
+
+    int * arr = getarr(10);
+
+    free(arr);
     return 0;
 }
 
