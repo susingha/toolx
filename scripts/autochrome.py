1131c0bda7ff06b19fe0fcd9cb9f7884351d2331
Supratik <supratik.dnit@gmail.com>
2018-11-22 03:07:15 -0800

    Update autochrome.py

--- scripts/autochrome.py
+++ scripts/autochrome.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 import pickle
 import time
 import sys
+import datetime
 
 ### script hosted at: https://github.com/susingha/toolx/blob/master/scripts/autochrome.py
 
@@ -12,8 +13,10 @@ waitdef = 3  # seconds
 
 ### used on deployment server ###
 wedriverurl = "C:/Users/supratik/bin/chromedriver.exe"
-norecsruntime = 100
+norecsruntime = 1000
 waitdef = 60 # seconds
+
+
 
 print (sys.version)
 options = webdriver.ChromeOptions()
@@ -28,7 +31,7 @@ print "Loading cookies"
 for cookie in pickle.load(open("QuoraCookies.pkl", "rb")):
     driver.add_cookie(cookie)
 
-driver.refresh();
+driver.refresh()
 print "Make sure browser is logged into desired account. If not, login manually using username and password"
 x = raw_input("Press Enter to save cookies. x to exit: ")
 if x == 'x' or x == 'X' or x == 'q' or x == 'Q':
@@ -38,13 +41,32 @@ totaldest = 0
 totalrecs = 0
 norecs = 0
 wait = waitdef
+
+scrollpause = 3
+scrollerjs = "document.body.scrollHeight || document.documentElement.scrollHeight"
+
 while True:
+    print "Current date and time:"
+    print str(datetime.datetime.now())
+    print
     print "About to remove more recommendations in", wait, "seconds"
-    driver.refresh();
+    driver.refresh()
     time.sleep(wait)
 
-    print "Saving Cookies"
+    print "Saving cookies"
     pickle.dump(driver.get_cookies() , open("QuoraCookies.pkl","wb"))
+
+    print "Scroll page to bottom"
+    scroll = True
+    lastheight = driver.execute_script("return " + scrollerjs)
+    while scroll:
+        driver.execute_script("window.scrollTo(0, " + scrollerjs + ")")
+        time.sleep(scrollpause)
+
+        newheight = driver.execute_script("return " + scrollerjs)
+        if (newheight == lastheight):
+            scroll = False
+        lastheight = newheight
 
     print "Finding recommendations"
     destroyrec = driver.find_elements_by_xpath("//button[@id='button' and @class='style-scope yt-icon-button' and @aria-label='Not interested']")
@@ -53,7 +75,7 @@ while True:
     if totalrecs == 0:
         print "No recommendations found"
         norecs += 1
-        wait += 1
+        wait += 5
         if norecs >= norecsruntime:
             break
     else:
@@ -69,4 +91,6 @@ while True:
 driver.close()
 print "Removed", totaldest, "recommendations"
 print "Congarts, you have defeated Youtube and Google"
-
+print
+print "Current date and time:"
+print str(datetime.datetime.now())
