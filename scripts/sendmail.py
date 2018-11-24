9732ca43c8a26434e09d42d2afc7382fffb3c1fd
Supratik <supratik.dnit@gmail.com>
2018-11-23 18:34:05 -0800

    Create sendmail.py

---
+++ scripts/sendmail.py
@@ -0,0 +1,27 @@
+import smtplib
+
+def sendamail(toaddrs):
+    username = 'notuberecs@gmail.com'
+    password = 'harjagah12'
+    fromaddr = username
+
+    server_ssl = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    server_ssl.ehlo()
+    server_ssl.login(username, password)
+    server_ssl.sendmail(fromaddr, toaddrs, msg)
+    server_ssl.close()
+
+toaddrs  = 'supratik.dnit@gmail.com'
+msg = "\r\n".join([
+  "From: notuberecs@gmail.com",
+  "To: supratik.dnit@gmail.com",
+  "Subject: Just a message",
+  "",
+  "Why, oh why"
+  ])
+
+sendamail(toaddrs)
+
+
+
+
