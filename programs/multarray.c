806947c3fe3b6a2199e0a16dd3a98a50d340be0e
supratik <supratik.dnit@gmail.com>
2018-11-25 15:53:14 -0800

    product of all elements on left and right of array

--- programs/multarray.c
+++ programs/multarray.c
@@ -1,3 +1,18 @@
+/*
+ * Given an array of integers,
+ * return a new array such that
+ * each element at index i of the new array
+ * is the product of all the numbers in the original array
+ * except the one at i.
+ *
+ * For example, if our input was [1, 2, 3, 4, 5],
+ * the expected output would be [120, 60, 40, 30, 24].
+ * If our input was [3, 2, 1],
+ * the expected output would be [2, 3, 6].
+ *
+ * Dont use Division
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -13,38 +28,34 @@
 #define FALSE 0
 #define ENDS  ('\0')
 
-#define MEASURE_RUNTIME
 #ifdef MEASURE_RUNTIME
 clock_t _ts_;
 #endif
 
 #define RANDOM
 #ifdef RANDOM
-#define rnd() (rand()%100)
+#define rnd() (rand()%10)
 #else
 #define rnd() (dataset[dataindex++])
 #endif
-
-
 
 int * getarr(int n) {
     int * arr = calloc(n, sizeof(int));
     srand(time(NULL));
     while (n--) {
-	arr[n] = rnd();
+	arr[n] = n+1;
     }
     printf("\n");
     return arr;
 }
 
-void shoarr(int arr[], int n) {
+void showarr(int arr[], int n) {
     int i;
     for (i = 0; i < n; ++i) {
 	printf("%d, ", arr[i]);
     }
     printf("\n");
 }
-
 
 int main (int argc, char *argv[])
 {
@@ -60,15 +71,41 @@ int main (int argc, char *argv[])
     unsigned int _i_ = 0, _j_;
 #endif
 
-    int * arr = getarr(10);
-    shoarr(arr, 10);
-    free(arr);
+
+    int len = 5, product = 1;;
+    int * arr1 = getarr(len);
+    int * arr2 = getarr(len);
+
+    showarr(arr1, len);
+
+    int i;
+    for (i = 0; i < len; ++i) {
+	arr2[i] = 1;
+    }
+
+    product = 1;
+    for (i = 1; i < len; ++i) {
+	product = product * arr1[i - 1];
+	arr2[i] = product;
+    }
+
+    product = 1;
+    for (i = len-2; i >= 0; --i) {
+	product = product * arr1[i + 1];
+	arr2[i] = product * arr2[i];
+    }
+
+    showarr(arr2, len);
+
+    free(arr2);
+    free(arr1);
+
+
 
 #ifdef MEASURE_RUNTIME
     _ts_ = clock() - _ts_;
     printf("Counted %u to %u in %f seconds\n", _j_, _i_, ((double)_ts_) / CLOCKS_PER_SEC); 
 #endif
-
     return 0;
 }
 
