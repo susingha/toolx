91d4172bbc83daa5a8a59b482479531b074fca59
supratik <supratik.dnit@gmail.com>
2019-01-08 14:37:26 -0800

    simplify quicksort in python

--- programs/quicksort.py
+++ programs/quicksort.py
@@ -1,76 +1,48 @@
-import sys
 import random
-
-
-def swapindex(index1, index2, array):
-    # print ("swap " + str(index1) + ", " + str(index2))
-    temp = array[index1]
-    array[index1] = array[index2]
-    array[index2] = temp
-
-def printarray(array, total):
-    print(array)
-
 
 def checksorted(array):
     for i in range(len(array) - 1):
         if (array[i] > array[i + 1]):
-            print("sup: Array not sorted")
+            print("Array is not sorted")
             return
-    print("sup: Array sorted")
+    print("Array is sorted")
 
 
-def sort(start, end, arr):
-    if (start == end):
-        return
+def sort(array, b, e):
 
-    final = 0
-    """
-    pivot = start
-    """
-    pivot = (start + end) / 2
-    leftprobe = start
-    rightprobe = end
+    # Finding Pivot
+    # Take last element as pivot
+    # Assume everything is more or equal to the pivot
+    # initialize hold at 0 (indicates first element of higher elements)
+    # initialize probe at 0
+    piv = e
+    hold = b
+    for probe in range(b, e):
+        if array[probe] < array[piv]:
+            array[probe], array[hold] = array[hold], array[probe]
+            hold += 1
 
-    while (leftprobe < rightprobe):
-        while (arr[leftprobe] <= arr[pivot] and leftprobe < end):
-            leftprobe += 1
-        while (arr[rightprobe] >= arr[pivot] and start < rightprobe):
-            rightprobe -= 1
+    # placing pivot
+    array[piv], array[hold] = array[hold], array[piv]
 
-        if (leftprobe < rightprobe):
-            swapindex(leftprobe, rightprobe, arr)
-
-    if (pivot < rightprobe):
-        final = rightprobe
-    elif (pivot > leftprobe):
-        final = leftprobe
-    else:
-        final = pivot
-
-    swapindex(pivot, final, arr)
-
-    if (start < final):
-        sort(start, final - 1, arr)
-    if (final < end):
-        sort(final + 1, end, arr)
+    # recursion
+    if b < hold-1:
+        sort(array, b, hold-1)
+    if hold+1 < e:
+        sort(array, hold+1, e)
 
 
-total = 100
+total = 10
 arr = []
-for i in range(0, total):
-    num = random.randint(1,10)
+for i in range(total):
+    num = random.randint(1,100)
     arr.append(num)
-    # print(str(i) + " - " + str(num))
 
-printarray(arr, total)
+print arr
 checksorted(arr)
-sort(0, len(arr)-1, arr)
-printarray(arr, total)
+sort (arr, 0, total-1)
+print arr
 checksorted(arr)
 
-print ("\n")
-print ("Python current version:")
-print (sys.version)
-print (sys.version_info)
-print (sys.hexversion)
+
+
