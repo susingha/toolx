d8faf39a78d6a22a6292489954f111feddb6f00c
supratik <supratik.dnit@gmail.com>
2019-02-04 02:26:12 -0800

    adding kmp

--- programs/heapsort.py
+++ programs/heapsort.py
@@ -3,11 +3,42 @@
 arr = [30, 91, 15, 72, 61, 41, 10, 37, 98, 41]
 lenarr = len(arr)
 
+def sorted(arr):
+    prev = None
+    for i in arr:
+        if prev and prev > i:
+            print "Not Sorted"
+            return False
+        prev = i
+    print "OK"
+    return True
+
 def leftchild(i):
     return 2*i + 1
 
 def rightchild(i):
     return 2*i + 2
+
+def maxheapify_itr(i, lent):
+    max = -1
+
+    done = False
+    while not done:
+
+        max = i
+        lc = leftchild(i)
+        rc = rightchild(i)
+        if lc < lent and arr[lc] > arr[i]:
+            max = lc
+        if rc < lent and arr[rc] > arr[max]:
+            max = rc
+
+        if i == max:
+            done = True
+        else:
+            arr[i], arr[max] = arr[max], arr[i]
+            i = max
+
 
 def maxheapify(i, lent):
     max = i
@@ -25,10 +56,11 @@ def maxheapify(i, lent):
 
 
 for i in xrange(lenarr):
-    maxheapify(lenarr-1-i, lenarr)
+    maxheapify_itr(lenarr-1-i, lenarr)
 
 for i in xrange(lenarr):
-    maxheapify(0, lenarr-i)
+    maxheapify_itr(0, lenarr-i)
     arr[0], arr[lenarr-1-i] = arr[lenarr-1-i], arr[0]
 
 print arr
+sorted(arr)
