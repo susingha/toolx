d60d20868ae9a13679541eb1d65ae9c8e1d80d65
supratik singha <supratik.dnit@gmail.com>
2019-05-29 00:37:43 -0700

    adding zombie program and a character device driver

---
+++ programs/zombie.c
@@ -0,0 +1,27 @@
+/*
+ * A C program to demonstrate Zombie Process.  
+ * Child becomes Zombie as parent is sleeping 
+ * when child process exits. 
+ */
+
+#include <stdlib.h> 
+#include <sys/types.h> 
+#include <unistd.h> 
+
+
+int main() 
+{ 
+    pid_t child_pid = fork(); 
+    unsigned int i = 0;
+  
+    if (child_pid > 0) { // Parent process  
+
+	while (1) while(--i); 
+  
+    } else {             // Child process 
+
+        exit(0);
+    }
+  
+    return 0; 
+} 
