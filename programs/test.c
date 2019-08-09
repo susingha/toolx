a4e93226c0b39634200fdcfd3a777f8af4bbc4c9
Supratik Singha <susingha@susingha-mac.local>
2019-08-09 16:13:04 -0700

    new programs

--- programs/test.c
+++ programs/test.c
@@ -1,64 +1,36 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <time.h>
 
-#if 0
-#include <string.h>
-#include <stdint.h>
-#include <sys/types.h>
-#endif
-
-#define TRUE  1
-#define FALSE 0
-#define ENDS  ('\0')
-
-#define MEASURE_RUNTIME
-#ifdef MEASURE_RUNTIME
-clock_t _ts_;
-#endif
-
-#define RANDOM
-#ifdef RANDOM
-#define rnd() (rand()%100)
-#else
-#define rnd() (dataset[dataindex++])
-#endif
-
-int * getarr(int n) {
-    int * arr = calloc(n, sizeof(int));
-    srand(time(NULL));
-    while (n--) {
-	arr[n] = rnd();
-	printf("%d, ", arr[n]);
-    }
-    printf("\n");
-    return arr;
-}
-
-
-int main (int argc, char *argv[])
-{
-    int _c_;
-    for (_c_ = 0; _c_ < argc; ++_c_) {
-	printf("%s ", argv[_c_]);
-    }
-    printf("(%d)\n", argc);
-
-#ifdef MEASURE_RUNTIME
-    _ts_ = clock();
-    unsigned int _i_ = 0, _j_;
-    _j_ = --_i_;
-    while(--_i_);
-    _ts_ = clock() - _ts_;
-    printf("Counted %u to %u in %f seconds\n", _j_, _i_, ((double)_ts_) / CLOCKS_PER_SEC); 
-#endif
-
-    int * arr = getarr(10);
-
-    free(arr);
-    return 0;
-}
-
-
-
+// C program of above approach 
+#include<stdio.h> 
+#include<string.h> 
+  
+// A utility function to get max of two integers 
+int max (int x, int y) { return (x > y)? x : y; } 
+  
+// Returns the length of the longest palindromic subsequence in seq 
+int lps(char *seq, int i, int j) 
+{ 
+   // Base Case 1: If there is only 1 character 
+   if (i == j) 
+     return 1; 
+  
+   // Base Case 2: If there are only 2 characters and both are same 
+   if (seq[i] == seq[j] && i + 1 == j) 
+     return 2; 
+  
+   // If the first and last characters match 
+   if (seq[i] == seq[j]) 
+      return lps (seq, i+1, j-1) + 2; 
+  
+   // If the first and last characters do not match 
+   return max( lps(seq, i, j-1), lps(seq, i+1, j) ); 
+} 
+  
+/* Driver program to test above functions */
+int main() 
+{ 
+    char seq[] = "zabcz"; 
+    int n = strlen(seq); 
+    printf ("The length of the LPS is %d", lps(seq, 0, n-1)); 
+    getchar(); 
+    return 0; 
+} 
